f456723576e082401d125e3e57cd51375d466a19
17380339 <densolo777@gmail.com>
2022-09-05 16:19:49 +0300

    added conditional links potential

--- project/scripts/parseStory.gd
+++ project/scripts/parseStory.gd
@@ -9,6 +9,7 @@ var currentPassage = 0
 var container
 var linkLabels : Array
 var currentLabel
+var conditions : Array
 
 onready var settingsMenu = $Popup
 
@@ -41,7 +42,8 @@ func load_story():
 func set_mainstory_text():
 	mainStoryLabel.text = data.passages[currentPassage].text.split("[")[0]
 	mainStoryLabel.percent_visible = 0
-	text_draw_speed = settings_var.draw_coef / mainStoryLabel.text.length()
+	if mainStoryLabel.text.length() != 0:
+		text_draw_speed = settings_var.draw_coef / mainStoryLabel.text.length()
 
 func process_links():
 	var passage = data.passages[currentPassage]
@@ -49,17 +51,35 @@ func process_links():
 	if "links" in passage:
 		var links = data.passages[currentPassage].links
 		for link in links:
-			var textLabel = robotoLabel.instance()
-			textLabel.anchor_left = 0
-			textLabel.anchor_right = 1
-			textLabel.connect("mouse_entered", textLabel, "_on_Mouse_entered")
-			textLabel.connect("mouse_exited", textLabel, "_on_Mouse_exited")
-			var linkPassage = find_by_pid(link.pid)
-			textLabel.connect("gui_input", self, "_on_Gui", [linkPassage])
-			textLabel.text = linkPassage.name
-			textLabel.percent_visible = 0
-			container.add_child(textLabel)
-			linkLabels.append(textLabel)
+			var should_show = true
+			var should_add = false
+			var str_res
+			var regex = RegEx.new()
+			regex.compile("{{(.*?)}}")
+			var results = regex.search_all(link.name)
+			if results:
+				for res in results:
+					str_res = res.get_string()
+					var st = res.get_start()
+					if st - str_res.length() > 0:
+						should_add = true
+					else:
+						if not str_res in conditions:
+							should_show = false
+			if should_show:
+				var textLabel = robotoLabel.instance()
+				textLabel.anchor_left = 0
+				textLabel.anchor_right = 1
+				textLabel.connect("mouse_entered", textLabel, "_on_Mouse_entered")
+				textLabel.connect("mouse_exited", textLabel, "_on_Mouse_exited")
+				var linkPassage = find_by_pid(link.pid)
+				if should_add:
+					linkPassage.trigger = str_res
+				textLabel.connect("gui_input", self, "_on_Gui", [linkPassage])
+				textLabel.text = regex.sub(link.name, "", true)
+				textLabel.percent_visible = 0
+				container.add_child(textLabel)
+				linkLabels.append(textLabel)
 		currentLabel = 0
 		
 		
@@ -75,6 +95,8 @@ func _on_Gui(event, linkPassage):
 	if linkLabels[linkLabels.size() - 1].percent_visible != 1:
 		return
 	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
+		if "trigger" in linkPassage:
+			conditions.append(linkPassage.trigger)
 		currentPassage = int(linkPassage.pid) - 1
 		set_mainstory_text()
 		for child in container.get_children():
@@ -90,13 +112,14 @@ func _process(delta):
 func process_draw_speed(delta):
 	if mainStoryLabel.percent_visible != 1:
 		mainStoryLabel.percent_visible += text_draw_speed * delta
-	if mainStoryLabel.percent_visible == 1:
-		text_draw_speed = settings_var.draw_coef / linkLabels[currentLabel].text.length()
-	if mainStoryLabel.percent_visible == 1 and linkLabels[currentLabel].percent_visible != 1:
-		linkLabels[currentLabel].percent_visible += text_draw_speed * delta
-	if linkLabels[currentLabel].percent_visible == 1 and currentLabel != linkLabels.size() - 1:
-		currentLabel += 1
-		text_draw_speed = settings_var.draw_coef / linkLabels[currentLabel].text.length()
+	if not linkLabels.empty():
+		if mainStoryLabel.percent_visible == 1:
+			text_draw_speed = settings_var.draw_coef / linkLabels[currentLabel].text.length()
+		if mainStoryLabel.percent_visible == 1 and linkLabels[currentLabel].percent_visible != 1:
+			linkLabels[currentLabel].percent_visible += text_draw_speed * delta
+		if linkLabels[currentLabel].percent_visible == 1 and currentLabel != linkLabels.size() - 1:
+			currentLabel += 1
+			text_draw_speed = settings_var.draw_coef / linkLabels[currentLabel].text.length()
 
 
 func _on_PalladaProject_gui_input(event):
@@ -106,7 +129,7 @@ func _on_PalladaProject_gui_input(event):
 func process_percent_visible():
 	if mainStoryLabel.percent_visible != 1:
 		mainStoryLabel.percent_visible = 1
-	else: 
+	elif not linkLabels.empty(): 
 		linkLabels[currentLabel].percent_visible = 1
 	
 func _on_SettingsButton_pressed():
